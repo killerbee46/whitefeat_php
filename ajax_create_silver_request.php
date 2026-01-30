7177ba2eee6255dc545f49bbbb21b1b9241a6cf3
KillaB <sugam@beeaver.co>
2026-01-30 09:45:31 +0545

    fix: html remoived

--- ajax_create_silver_request.php
+++ ajax_create_silver_request.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
     <?php
     include 'db_connect.php';
     include 'ajax_cookie.php';
@@ -63,6 +53,3 @@
     }
 
     ?>
-</body>
-
-</html>
