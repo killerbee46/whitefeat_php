681f3e2750469a8b5aaf9e702bffe4360911b564
KillaB <sugam@beeaver.co>
2026-02-25 00:06:53 +0545

    feat: offer time

--- product.php
+++ product.php
@@ -34,9 +34,13 @@
   $displayuser = mysqli_query($con, $sqluser);
   $rowuser = mysqli_fetch_array($displayuser);
   if ($GLOBALS['customer'] != 0) {
+    echo '<script>
+    alert("'.$rowuser['role'].'");
+    window.location.href = "/";
+    </script>';
     if ($rowuser['role'] < 3 && $productname == 1849) {
       echo '<script>
-    alert("Cannot View Or Order Product'.$rowuser['role'].'");
+    alert("Cannot View Or Order Product");
     window.location.href = "/";
     </script>';
     }
