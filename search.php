c135487f210bcabc5eaee351e38960f24e8d1581
KillaB <sugam@beeaver.co>
2026-03-08 13:49:40 +0545

    feat: no term exclusion handler when not present

--- search.php
+++ search.php
@@ -20,7 +20,7 @@
 
 		if (isset($_GET['term'])) {
 			$title = $_GET['term'];
-			$exclude = $_GET["termNot"];
+			$exclude = $_GET["termNot"] ?? "";
 		} else {
 			if (isset($_GET['tags'])) {
 				foreach ($_GET['tags'] as $key => $tag) {
